998ca81c82836feecc2b9c2cde610161ae76afed
Luigi Moraes <luigimoraes3@gmail.com>
2022-10-24 12:10:34 -0300

    Using Vuetify 2 and Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,55 @@
 <template>
-	<nav>
-		<button>Menu</button>
-		<img src="" alt="twitter logo"/>
-	</nav>
-	<main>
-		<PostComponent/>
-	</main>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import PostComponent from './components/Post.vue'
 
 export default {
   name: 'App',
-  components: { PostComponent }
-}
+
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap');
-
-#app {
-  font-family: "Montserrat", Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-	
-}
-
-nav {
-	display: flex;
-	justify-content: space-between;
-	margin-bottom: 2vh;
-}
-</style>
