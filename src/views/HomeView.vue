badd83c00cd680845ac911aecc83e4cc17a27207
Luigi Moraes <luigimoraes3@gmail.com>
2022-10-26 21:40:52 -0300

    Problems with this fucking dotenv

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
   <v-container id="main">
-    <PostView 
-      name="Luigi" 
-      userName="@luigi.dev"
-      text="Hello World"
+    <PostView v-for="(t, i) in tweets" :key="i"
+      name=authors[i].name 
+      userName=authors[i].username
+      text=t.text
       likes=420
       retweets=69
       comments=13
@@ -14,9 +14,23 @@
 
 <script>
   import PostView from '../components/Post.vue';
- 
+  import { getTweets } from '../../apiConnect.js';
+
   export default {
     name: 'MainView',
-    components: { PostView }
+    components: { PostView },
+    data() {
+      return { 
+        tweets: [],
+        authors: [],
+        errorHappend: false,
+        errorMsg: ""
+      }
+    },
+    mounted(){
+      getTweets()
+        .then(res => console.log(res))
+        .catch(rej => console.error(rej));
+    }
   }
 </script>
