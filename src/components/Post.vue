1a0cc9de62a6133ddd33012311f3d152b5fb2e55
Luigi Moraes <luigimoraes3@gmail.com>
2022-10-28 00:17:23 -0300

    Added avatars to posts

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,18 +1,15 @@
 <template>
  <v-container class="content">
   <v-row>
-   <v-col cols="3">
-		<figure>
-     <v-img class="user-img" 
-      :src="imgSrc" 
-      :alt="userName+' profile photo'">
-    </v-img>
-    </figure>
+   <v-col cols="2">
+		<v-avatar size="55">
+     <v-img :src="imgSrc" :alt="userName+' profile photo'"></v-img>
+    </v-avatar>
    </v-col>
-   <v-col cols="9">
+   <v-col cols="10">
 		<header>
      <h2 class="name">{{ name }}</h2>
-     <h3 class="user-name">{{ userName }}</h3>
+     <h3 class="user-name">@{{ userName.toLowerCase() }}</h3>
     </header>
     <p class="text-md-body-1 text-no-wrap">{{ text }}</p>
     <div class="buttons">
@@ -96,14 +93,11 @@ header {
 	align-items: center;
 }
 
-figure {
-  height: 100%;
+.avatar {
 	display: flex;
   align-items: center;
-  margin-right: 2vw;
+  justify-content: center;
 }
-
-.user-img { border: 1px solid black; }
 
 p { margin-top: 3%; }
 
